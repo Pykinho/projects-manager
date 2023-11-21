e224b16548ec1a890bad1818d6a8fba9b7fa5839
Pykinho <miko.pyka@gmail.com>
2023-11-21 15:02:42 +0100

    Input validation and making projects visible

--- projects-manager/src/App.jsx
+++ projects-manager/src/App.jsx
@@ -11,26 +11,47 @@ function App() {
 
   function handleStartAddProject() {
     setProjectState((prevState) => {
-      return { selectedProjectId: null, projects: prevState.projects };
+      return { ...prevState, selectedProjectId: null };
     });
   }
 
-  function cancelAddProject() {
+  function handleCancelAddProject() {
     setProjectState((prevState) => {
-      return { selectedProjectId: undefined, projects: prevState.projects };
+      return { ...prevState, selectedProjectId: undefined };
+    });
+  }
+  function handleSaveAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
     });
   }
 
   let content;
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onCancel={cancelAddProject} />;
+    content = (
+      <NewProject
+        onCancel={handleCancelAddProject}
+        onSave={handleSaveAddProject}
+      />
+    );
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoSelection onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar onStartAddProject={handleStartAddProject} />
+      <Sidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
--- projects-manager/src/components/Sidebar.jsx
+++ projects-manager/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button";
 
-export default function Sidebar({ onStartAddProject }) {
+export default function Sidebar({ onStartAddProject, projects }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-950 text-stone-100 md:w-72 rounded-r-xl">
       <h2 className="mb-9 font-bold uppercase md:text-xl text-stone-200">
@@ -8,7 +8,15 @@ export default function Sidebar({ onStartAddProject }) {
       </h2>
 
       <Button onClick={onStartAddProject}>Add project</Button>
-      <ul></ul>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-100 hover:bg-stone-900">
+              {project.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 }
